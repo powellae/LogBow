31a97c6c8850c75cf6d14fd03c523b55b04e14e2
hoode15 <35671715+hoode15@users.noreply.github.com>
2018-01-30 18:06:32 -0500

    Add files via upload

---
+++ ModelingProject1.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: emmahood
+ *
+ * Created on January 30, 2018, 2:29 PM
+ */
+
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+double function_1 (double b, double x)
+{
+  double m;
+  m = sqrt(pow((pow(18,2) + (pow(b,2)))/(2*b),2) - pow((x-18),2)) + ((pow(18,2) - pow(b,2))/(2*b));
+
+  return m;
+}
+
+int main ()
+{
+  double b, q [35], sum = 0;
+  int n, i;
+
+  cout << endl << "What is |b|? ";
+  cin >> b;
+  b = abs(b);
+  cout << endl;
+
+  for(n = 0; n <= 35; n++)
+  {
+    q[n] = abs(function_1(b, (n + 1)) - function_1(b, n));
+    cout << "Q" << n + 1 << ": " << q[n] << endl;
+    sum = sum + q[n];
+  }
+
+  cout << endl << endl << "Sum: " << sum/2;
+
+  cout << endl << endl << endl;
+
+  return 0;
+
+}
+
